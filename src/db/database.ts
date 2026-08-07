94c88a43f4017a94392f9095c3b798ece4a0c440
Jemal Khachidze <jkhachidze92@gmail.com>
2026-08-07 02:30:59 +0200

    feat: add database model and types

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,23 +1,33 @@
 import { Database, open } from 'sqlite';
 import sqlite3 from 'sqlite3';
-import path from 'node:path';
 
-const db_path = path.join(process.cwd(), 'data', 'trail-guide.db');
+function requireEnv(name: string): string {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`${name} is not set. Please specify it in your .env file`);
+  }
+  return value;
+}
 
 let db: Database | null = null;
 
 export async function connectDB() {
-  db = await open({ filename: db_path, driver: sqlite3.Database });
-  const testRegion = await db.get('SELECT * FROM regions LIMIT 1');
+  const dbPath = requireEnv('DB_PATH');
+  db = await open({ filename: dbPath, driver: sqlite3.Database });
+
+  const testRegion: unknown = await db.all('SELECT * FROM regions LIMIT 1');
+  const testTrail: unknown = await db.all('SELECT * FROM trails LIMIT 1');
 
   console.log(`$Database connected and tables initialized.`);
   console.log(JSON.stringify(testRegion, null, 2));
+  console.log(JSON.stringify(testTrail, null, 2));
+
   return db;
 }
 
 export function getDB(): Database {
   if (!db) {
-    throw new Error('Database not connected. Call connectToDatabase() first.');
+    throw new Error('Database not connected. Call connectDB() first.');
   }
   return db;
 }
