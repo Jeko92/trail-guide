2a3308a727de766fb66707ccf0a4960f59920f72
Jemal Khachidze <jkhachidze92@gmail.com>
2026-08-07 02:33:23 +0200

    feat: extend homepage controller, adjust template

--- src/controllers/public/home.controller.ts
+++ src/controllers/public/home.controller.ts
@@ -1,7 +1,18 @@
-import type { Request, Response } from 'express';
+import type { NextFunction, Request, Response } from 'express';
+import { getAllTrails } from '../../models/trails.model.ts';
+import type { TrailWithRegion } from '../../types/types.ts';
 
-const homeController = async (_req: Request, res: Response) => {
-  res.render('public/home.njk', { title: 'Trail Guide' });
+const homeController = async (_req: Request, res: Response, next:NextFunction) => {
+  try {
+    const trails:TrailWithRegion[] = await getAllTrails();
+    // console.log(JSON.stringify(trails));
+    res.render('public/home.njk', {
+      title: 'Trail Guide',
+      trails: trails,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default homeController;
